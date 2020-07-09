103888a5aa140eb4f88fc650aee00d83c4b0999c
kelwinv <kelwin08@gmail.com>
2020-07-09 12:30:09 -0300

    finalização da Addresses table

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -7,7 +7,7 @@ module.exports ={
     return res.json(response);
   },
   async store(req,res){
-    const {  name, email} = req.body;
+    const { name, email} = req.body;
 
     const user = await User.create({ name, email });
 
