3bc314a8f65ce242e0f12543512daac0ee049ea7
kelwinv <kelwin08@gmail.com>
2020-07-09 15:43:49 -0300

    finalização

---
+++ src/controllers/ReportController.js
@@ -0,0 +1,30 @@
+const{ Op } = require('sequelize');
+const User = require('../models/User');
+const { where } = require('sequelize');
+
+module.exports = {
+  async show(req,res){
+    const users = await User.findAll({
+      attributes: ['name','email'],
+      where: {
+        email: {
+          [Op.iLike]: '%@rocketseat.com.br',
+         },
+       },
+       include: [{  
+          association: 'addresses',
+          where: { street : 'rua guilherme'}
+        }, { 
+          association: 'techs',
+          required: false,
+          where: { 
+            name: {
+              [Op.iLike]: 'React%',
+            },
+          }, 
+        }
+       ]
+    })
+    return res.json(users);
+  }
+}
